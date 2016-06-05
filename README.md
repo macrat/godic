c3b2fb774b9f81f51d06b90b84b31e00622b4f6e
MacRat <m@crat.jp>
2016-06-05 17:31:09 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,23 @@
+godic
+=====
+
+goo国語辞典を見るためのコンソールアプリ。
+
+スクレイピングしているだけなので、使用はほどほどに。
+突然使えなくなることもあるかもしれません。
+
+## インストール
+``` bash
+	$ go get github.com/macrat/godic
+```
+
+## 使い方
+以下のようにして使用すると、マッチした単語に関する説明が表示されます。
+``` bash
+	$ godic 調べたい単語
+```
+
+オプション等は今のところありません。
+
+## ライセンス
+[MIT License](https://opensource.org/licenses/MIT) (c)2016 [MacRat](http://blanktar.jp)
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func main() {
+	resp, err := http.Get(fmt.Sprintf(
+		"http://dictionary.goo.ne.jp/srch/jn/%s/m6u/",
+		strings.Replace(url.QueryEscape(os.Args[1]), "+", "%20", -1),
+	))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	html, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	candidates := html.Find(".list-search-a > li")
+	if candidates.Length() >= 1 {
+		candidates.Each(func(i int, s *goquery.Selection) {
+			fmt.Println(s.Find(".title").Text())
+			fmt.Printf(
+				"\033[37m%s\033[0m\n",
+				strings.TrimSpace(strings.Replace(s.Find(".mean").Text(), "\t", "\n", -1)),
+			)
+			fmt.Println()
+		})
+	} else {
+		explanation := html.Find(".explanation").Text()
+		fmt.Println(strings.TrimSpace(strings.Replace(explanation, "\t", "", -1)))
+	}
+}
